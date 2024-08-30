634294eeab3e4cc80b2d79146d0fc7984c8b4552
Trần Bảo Nguyên <nguyentb.22bi13342@usth.edu.vn>
2024-08-30 11:18:56 +0700

    prac3+4

--- prac/app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
+++ prac/app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
@@ -2,28 +2,36 @@ package vn.edu.usth.weatheractivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "Weather";
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Log.i(TAG, "On Create");
-    };
+        setContentView(R.layout.activity_main); // Ensure this layout has a FrameLayout with ID 'container'
+
+        // Create a new instance of ForecastFragment
+        ForecastFragment forecastFragment = new ForecastFragment();
+
+        // Add the fragment to the 'container' FrameLayout dynamically
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, forecastFragment) // Ensure R.id.container exists in activity_weather.xml
+                .commit();
+
+        textView = findViewById(R.id.textView);
+
+
+    }
+
 
     @Override
     protected void onStart(){
