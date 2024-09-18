9bad3f43ef036116d4fa5ef670b5ec191e451ed2
Trần Bảo Nguyên <nguyentb.22bi13342@usth.edu.vn>
2024-09-18 20:17:38 +0700

    prac8+9

--- app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
@@ -16,5 +18,8 @@ public class WeatherActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.view_pager);
         WeatherPageAdapter adapter = new WeatherPageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/vn/edu/usth/weatheractivity/WeatherPageAdapter.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/WeatherPageAdapter.java
@@ -23,4 +23,19 @@ public class WeatherPageAdapter extends FragmentPagerAdapter {
         // You want 3 instances of WeatherAndForecastFragment
         return 3;
     }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Return the title for each tab
+        switch (position) {
+            case 0:
+                return "Hanoi";
+            case 1:
+                return "Paris";
+            case 2:
+                return "Toulouse";
+            default:
+                return null;
+        }
+    }
 }
